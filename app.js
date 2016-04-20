975174fe77e7b7398b5ddec5274b9d560d4b863d
meiordac <matias.iordache@gmail.com>
2016-04-20 18:48:53 -0300

    removed hardcoded data in the controller

--- app.js
+++ app.js
@@ -1,8 +1,8 @@
 var app = angular.module('flapperNews',[]);
 
 app.factory('posts', [function(){
-  // service body
   var _posts={posts:[]};
+  
   return _posts;
 }])
 
@@ -10,16 +10,8 @@ app.controller('MainCtrl', [
 '$scope',
 'posts',
 function($scope, posts){
-	$scope.test='Hello world!';
 
-	$scope.posts = [
-	{title: 'post 1', upvotes: 5},
-	{title: 'post 2', upvotes: 2},
-	{title: 'post 3', upvotes: 1},
-	{title: 'post 4', upvotes: 7},
-	{title: 'post 5', upvotes: 3},
-	{title: 'post 6', upvotes: 4}
-	];
+	$scope.posts = posts.posts;
 
 	$scope.addPost= function()
 	{
