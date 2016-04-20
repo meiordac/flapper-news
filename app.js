3b76b2fc6b2a4d908d22167f1ca455fc0e8d2f91
meiordac <matias.iordache@gmail.com>
2016-04-20 17:49:34 -0300

    add button to add new posts

--- app.js
+++ app.js
@@ -3,22 +3,20 @@ app.controller('MainCtrl', [
 '$scope',
 function($scope){
 	$scope.test='Hello world!';
+
 	$scope.posts = [
 	{title: 'post 1', upvotes: 5},
-
 	{title: 'post 2', upvotes: 2},
-
 	{title: 'post 3', upvotes: 1},
-
 	{title: 'post 4', upvotes: 7},
-
 	{title: 'post 5', upvotes: 3},
-
 	{title: 'post 6', upvotes: 4}
-
 	];
+
+	$scope.addPost= function()
+	{
+		$scope.posts.push({title: 'new post', upvotes: 0});
+	};
 }
-
-
 
 ]); 
