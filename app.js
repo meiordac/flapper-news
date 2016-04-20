062d6c17d0c752a79cb937b62e5165d1be6b3bb3
meiordac <matias.iordache@gmail.com>
2016-04-20 18:00:15 -0300

    clear title after add post

--- app.js
+++ app.js
@@ -15,7 +15,8 @@ function($scope){
 
 	$scope.addPost= function()
 	{
-		$scope.posts.push({title: 'new post', upvotes: 0});
+		$scope.posts.push({title: $scope.title, upvotes: 0});
+		$scope.title='';
 	};
 }
 
