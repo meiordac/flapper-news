0f436c2c5502708494fd428f2b20ff3a1d8c1ce8
meiordac <matias.iordache@gmail.com>
2016-04-21 11:31:34 -0300

    add inline posts

--- app.js
+++ app.js
@@ -34,6 +34,18 @@ app.controller('PostsCtrl'[
 	'posts',
 	function($scope, $stateParams, posts){
 
+		$scope.post=posts.posts[$stateParams.id];
+
+		$scope.addComment = function(){
+			if($scope.body === '') { return; }
+			$scope.post.comments.push({
+				body: $scope.body,
+				author: 'user',
+				upvotes: 0
+			});
+			$scope.body = '';
+}; 
+
 	}]);
 
 app.controller('MainCtrl', [
