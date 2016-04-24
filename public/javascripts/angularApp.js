1d24d41bda8b947c6196477b2ebe6c3cfe6f13f2
meiordac <matias.iordache@gmail.com>
2016-04-24 14:32:48 -0300

    add navbar controller and navbar to html code

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -227,3 +227,13 @@ function($scope, $state, auth){
     });
   };
 }]);
+
+
+app.controller('NavCtrl', [
+'$scope',
+'auth',
+function($scope, auth){
+  $scope.isLoggedIn = auth.isLoggedIn;
+  $scope.currentUser = auth.currentUser;
+  $scope.logOut = auth.logOut;
+}]);
