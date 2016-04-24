fea0fe212a71f855aa087489d129bd5eecfaf25b
meiordac <matias.iordache@gmail.com>
2016-04-24 14:34:58 -0300

    add auth to posts factory

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -54,7 +54,7 @@ app.config([
 		$urlRouterProvider.otherwise('home');
 	}]);
 
-app.factory('posts', ['$http', function($http){
+app.factory('posts', ['$http', 'auth', function($http, auth){
 
 	var o = {
     posts: []
@@ -66,28 +66,34 @@ app.factory('posts', ['$http', function($http){
     });
   };
 
-    o.create = function(post) {
-  return $http.post('/posts', post).success(function(data){
+o.create = function(post) {
+  return $http.post('/posts', post, {
+    headers: {Authorization: 'Bearer '+auth.getToken()}
+  }).success(function(data){
     o.posts.push(data);
-    });
-  };
-
-  o.addComment = function(id, comment) {
-  return $http.post('/posts/' + id + '/comments', comment);
+  });
 };
 
 o.upvote = function(post) {
-  return $http.put('/posts/' + post._id + '/upvote')
-    .success(function(data){
-      post.upvotes += 1;
-    });
+  return $http.put('/posts/' + post._id + '/upvote', null, {
+    headers: {Authorization: 'Bearer '+auth.getToken()}
+  }).success(function(data){
+    post.upvotes += 1;
+  });
+};
+
+o.addComment = function(id, comment) {
+  return $http.post('/posts/' + id + '/comments', comment, {
+    headers: {Authorization: 'Bearer '+auth.getToken()}
+  });
 };
 
 o.upvoteComment = function(post, comment) {
-  return $http.put('/posts/' + post._id + '/comments/'+ comment._id + '/upvote')
-    .success(function(data){
-      comment.upvotes += 1;
-    });
+  return $http.put('/posts/' + post._id + '/comments/'+ comment._id + '/upvote', null, {
+    headers: {Authorization: 'Bearer '+auth.getToken()}
+  }).success(function(data){
+    comment.upvotes += 1;
+  });
 };
 
 o.get = function(id) {
