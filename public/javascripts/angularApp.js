d53fe85a5e9a6f67303636a86c7ff016bc040430
meiordac <matias.iordache@gmail.com>
2016-04-24 14:30:22 -0300

    add register and login states and html

--- public/javascripts/angularApp.js
+++ public/javascripts/angularApp.js
@@ -28,6 +28,28 @@ app.config([
 			}]
 		}
 	});
+		$stateProvider
+		.state('login', {
+			url: '/login',
+			templateUrl: '/login.html',
+			controller: 'AuthCtrl',
+			onEnter: ['$state', 'auth', function($state, auth){
+				if(auth.isLoggedIn()){
+					$state.go('home');
+				}
+			}]
+		});
+		$stateProvider
+		.state('register', {
+			url: '/register',
+			templateUrl: '/register.html',
+			controller: 'AuthCtrl',
+			onEnter: ['$state', 'auth', function($state, auth){
+				if(auth.isLoggedIn()){
+					$state.go('home');
+				}
+			}]
+		});
 
 		$urlRouterProvider.otherwise('home');
 	}]);
@@ -81,8 +103,53 @@ o.get = function(id) {
 app.factory('auth', ['$http', '$window', function($http, $window){
    var auth = {};
 
+auth.saveToken = function (token){
+  $window.localStorage['flapper-news-token'] = token;
+};
+
+auth.getToken = function (){
+  return $window.localStorage['flapper-news-token'];
+};
+
+auth.isLoggedIn = function(){
+  var token = auth.getToken();
+
+  if(token){
+    var payload = JSON.parse($window.atob(token.split('.')[1]));
+
+    return payload.exp > Date.now() / 1000;
+  } else {
+    return false;
+  }
+};
+
+auth.currentUser = function(){
+  if(auth.isLoggedIn()){
+    var token = auth.getToken();
+    var payload = JSON.parse($window.atob(token.split('.')[1]));
+
+    return payload.username;
+  }
+};
+
+auth.register = function(user){
+  return $http.post('/register', user).success(function(data){
+    auth.saveToken(data.token);
+  });
+};
+
+auth.logIn = function(user){
+  return $http.post('/login', user).success(function(data){
+    auth.saveToken(data.token);
+  });
+};
+
+auth.logOut = function(){
+  $window.localStorage.removeItem('flapper-news-token');
+};
+
   return auth;
-}])
+}]);
 
 app.controller('PostsCtrl', [
 	'$scope',
@@ -136,3 +203,27 @@ function($scope, posts){
 	};
 
 }]); 
+
+app.controller('AuthCtrl', [
+'$scope',
+'$state',
+'auth',
+function($scope, $state, auth){
+  $scope.user = {};
+
+  $scope.register = function(){
+    auth.register($scope.user).error(function(error){
+      $scope.error = error;
+    }).then(function(){
+      $state.go('home');
+    });
+  };
+
+  $scope.logIn = function(){
+    auth.logIn($scope.user).error(function(error){
+      $scope.error = error;
+    }).then(function(){
+      $state.go('home');
+    });
+  };
+}]);
